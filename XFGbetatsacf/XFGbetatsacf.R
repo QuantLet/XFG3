f4a107518d4c38a7172088f1e1664653abc4cce3
Shi Chen <shichen@Administrators-MacBook-Pro.local>
2015-11-21 23:46:09 +0100

    reformat R
    add blank

--- XFGbetatsacf/XFGbetatsacf.R
+++ XFGbetatsacf/XFGbetatsacf.R
@@ -17,21 +17,21 @@ plot(as.Date(as.character(ibm$date), "%Y%m%d"), ibm$beta, type = "l", xlab = "ti
      ylab = "beta", lwd = 2, col = "blue", ylim = c(0, 2))
 
 # add confidence internal of upper panel
-par(new = T)
+par(new   = T)
 plot(as.Date(as.character(ibm$date), "%Y%m%d"), ibm$beta + ibm$conf, type = "l", 
      xlab = "time", ylab = "beta", lwd = 1, col = "red", lty = 2, ylim = c(0, 2))
-par(new = T)
+par(new   = T)
 plot(as.Date(as.character(ibm$date), "%Y%m%d"), ibm$beta - ibm$conf, type = "l", 
      xlab = "time", ylab = "beta", lwd = 1, col = "red", lty = 2, ylim = c(0, 2))
 
 # plots the lower panel
-acfData = acf(ibm$beta, lag.max = 40, type = "correlation", plot = FALSE)
-conf    = 2/sqrt(length(ibm$beta))
+acfData   = acf(ibm$beta, lag.max = 40, type = "correlation", plot = FALSE)
+conf      = 2/sqrt(length(ibm$beta))
 plot(acfData$lag, acfData$acf, type = "h", xlab = "lag", ylab = "ACF", lwd = 2, col = "blue", 
      ylim = c(-0.3, 1))
-par(new = T)
+par(new   = T)
 plot(acfData$lag, array(data = conf, dim = length(acfData$lag)), type = "l", xlab = "lag", 
      ylab = "ACF", lwd = 1, col = "red", lty = 2, ylim = c(-0.3, 1))
-par(new = T)
+par(new   = T)
 plot(acfData$lag, array(data = -conf, dim = length(acfData$lag)), type = "l", xlab = "lag", 
      ylab = "ACF", lwd = 1, col = "red", lty = 2, ylim = c(-0.3, 1))
